4502d8511a3158f30c0122a4364fc51ac84fcc1c
GabrielLefevre <gabriel_lefevre@ens.univ-artois.fr>
2017-03-10 12:33:32 +0100

    integration FSObundle

--- src/AnimauxBundle/AnimauxBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace AnimauxBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class AnimauxBundle extends Bundle
-{
-}
---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: DEV2
+ * Date: 10/03/2017
+ * Time: 10:02
+ */
+
+namespace AppBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+
+
+}
